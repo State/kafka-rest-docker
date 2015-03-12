577b3c01c8fc09a2aac94442195bae2ebc57ab32
Dan Harvey <dan@state.com>
2015-03-12 11:40:33 +0000

    Switch to a simpler templating system with bash to prevent sed escape issues.

--- run.sh
+++ run.sh
@@ -5,23 +5,14 @@
 : ${SCHEMA_REGISTRY_URL}
 : ${ZOOKEEPER_URL}
 
-if [[ -n ${KAFKA_REST_INSTANCE_ID} ]]; then
-  sed -e "s/id=.*/id=${KAFKA_REST_INSTANCE_ID}/" \
-    -i /etc/kafka-rest/kafka-rest.properties
-fi
+cat >/etc/kafka-rest/kafka-rest.properties <<EOF
+id=${KAFKA_REST_INSTANCE_ID:-1}
+schema.registry.url=${SCHEMA_REGISTRY_URL:-http://localhost:8081}
+zookeeper.connect=${ZOOKEEPER_URL-localhost:2181}
+EOF
 
 if [[ -n ${ADVERTISED_HOSTNAME} ]]; then
   echo "host.name=${ADVERTISED_HOSTNAME}" >> /etc/kafka-rest/kafka-rest.properties
-fi
-
-if [[ -n ${SCHEMA_REGISTRY_URL} ]]; then
-  sed -e "s/schema.registry.url=.*/schema.registry.url=${SCHEMA_REGISTRY_URL}/" \
-    -i /etc/kafka-rest/kafka-rest.properties
-fi
-
-if [[ -n ${ZOOKEEPER_URL} ]]; then
-  sed -e "s/zookeeper.connect=.*/zookeeper.connect=${ZOOKEEPER_URL}/" \
-    -i /etc/kafka-rest/kafka-rest.properties
 fi
 
 exec ./bin/kafka-rest-start /etc/kafka-rest/kafka-rest.properties
